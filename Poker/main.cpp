4241731a6eb60a8bb2dc7f5bf5ab82f744f4a7ff
Sam Vercauteren <luxmesa@Sam-Vercauterens-MacBook.local>
2012-07-11 21:15:36 -0400

    Initial Commit

---
+++ Poker/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Poker
+//
+//  Created by Sam Vercauteren on 7/11/12.
+//  Copyright (c) 2012 First Flight High School. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
